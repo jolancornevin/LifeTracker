c7f88d9cf363b2a6c8a6867be126e871de9c4539
jolancornevin <cornevin.jolan@gmail.com>
2024-04-02 23:39:15 +0200

    add import from assets

--- app/components/settings/import_export.tsx
+++ app/components/settings/import_export.tsx
@@ -5,6 +5,8 @@ import { ExportToEmail } from '../../models/exporter';
 import { RealmContext } from '../../models/main';
 import { newDate } from '../../utils';
 
+import { customData } from '../../assets/data';
+
 const { useRealm } = RealmContext;
 
 export const ImportExport = () => {
@@ -91,7 +93,52 @@ export const ImportExport = () => {
 
 											obj.date = obj.date.getTime();
 										}
+									}
 
+									realm.create(type, obj);
+								});
+							});
+
+							setDone(true);
+						});
+					}}
+				/>
+			</View>
+
+			<Text>{done && 'done'}</Text>
+
+			<View style={{ paddingTop: 50 }}>
+				<Text>{'-------------'}</Text>
+
+				<Button
+					title={'Import from assets'}
+					color={'red'}
+					onPress={() => {
+						const values = customData;
+
+						realm.write(() => {
+							realm.deleteAll();
+
+							['EventSettings', 'Event', 'DayRating'].map((type) => {
+								values[type].forEach((obj) => {
+									if (obj['_id']) {
+										obj['_id'] = new Realm.BSON.ObjectId(obj['_id']);
+									}
+
+									// reset all hours to UTC 0.
+									// this code is not really going to be needed for the future,
+									// it's just that the old dates weren't fixed to hours 0
+									if (obj?.date) {
+										const correctedDate = new Date(obj.date);
+										if (correctedDate.getUTCHours() != 0) {
+											obj.date = newDate(
+												correctedDate.getUTCFullYear(),
+												correctedDate.getMonth(),
+												correctedDate.getUTCDate() + 1,
+											);
+
+											obj.date = obj.date.getTime();
+										}
 									}
 
 									realm.create(type, obj);
